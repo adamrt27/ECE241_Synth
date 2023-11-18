111aea8757f975b8c73908f21ff96b1ce8158c3e
candicerayne <150631261+candicerayne@users.noreply.github.com>
2023-11-18 16:36:12 -0500

    Add files via upload

---
+++ squarewave.v
@@ -0,0 +1,34 @@
+module square_wave_generator(
+	input clk,
+	input reset_n,
+    input [25:0]frequency,
+	output sq_wave
+);
+
+	localparam CLOCK_FREQUENCY = 50000000;
+	
+     reg [25:0] period;
+	reg sq_wave_reg = 0;
+	assign sq_wave = sq_wave_reg;
+
+ always @(posedge clk) begin
+		if (reset_n) begin
+			period <= 26'b0;
+			sq_wave_reg	<= 1'b0;
+            period <= 26'b0;
+		end
+	
+		else begin 
+			if (period == 26'b0) begin
+				sq_wave_reg <= ~sq_wave_reg;
+				period <= CLOCK_FREQUENCY / frequency - 1;
+				
+			
+			end 
+
+			else 
+				period <= period - 1; 
+			end
+		end
+		
+endmodule
