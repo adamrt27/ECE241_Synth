c99ee4f5fbb86016773f0f8863fecb361fa3eff6
taratafreshi <150408989+taratafreshi@users.noreply.github.com>
2023-11-18 17:57:06 -0500

    Update output.v

--- output.v
+++ output.v
@@ -1,23 +1,23 @@
-module sound (clock, reset, out);
+module sound (clock, reset, outs);
     input clock, reset;
-    output out;
+    output reg outs;
 parameter bits = 8;
 parameter frequencyDivider = 25;
 
 reg [7:0] counter;
-reg [31:0] generate;
+reg [31:0] generates;
 
 always@(posedge clock) begin
     if (reset) begin
         counter <= 0;
-        generate <=0;
+        generates <=0;
     end else begin
         counter <= counter + 1;
-        generate <= generate + frequencyDivider;
-        if ((generate[31] == 1))
-            out <= 1'b1;
+        generates <= generates + frequencyDivider;
+        if ((generates[31] == 1))
+            outs <= 1'b1;
         else 
-            out <= 1'b1;
-    end
+            outs <= 1'b0;
+	end
 end
 endmodule
