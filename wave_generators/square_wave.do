62721f1e9b3833b398954dc2f5b9fdf0f8b1296b
Adam Taback <122375166+adamrt27@users.noreply.github.com>
2023-11-25 15:49:09 -0500

    added amplitude to square_wave

---
+++ wave_generators/square_wave.do
@@ -0,0 +1,23 @@
+# set the working dir, where all compiled verilog goes
+
+vlib work
+
+# compile all verilog modules in mux.v to working dir
+# could also have multiple verilog files
+vlog square_wave_generator.v
+
+#load simulation using mux as the top level simulation module
+vsim square_wave_generator
+
+#log all signals and add some signals to waveform window
+log {/*}
+# add wave {/*} would add all items in top level simulation module
+add wave {/*}
+
+force {clk} 0 0 ns, 1 5ns -r 10ns
+
+# TestCases: C0 (16.35), B6 (1975.53), A#7 (3729.31)
+force {reset_n} 0 0 ns, 1 10 ns
+force {frequency} 16'd10000 0 ns
+force {amplitude} 6'd31;
+run 20 ms
--- wave_generators/square_wave_generator.v
+++ wave_generators/square_wave_generator.v
@@ -2,25 +2,29 @@ module square_wave_generator(
 	input clk,
 	input reset_n,
     input [15:0]frequency,
-	output sq_wave
+	input [5:0] amplitude; // amplitude from 0 to peak (not peak to peak)
+	output [6:0] sq_wave
 );
 
 	localparam CLOCK_FREQUENCY = 50000000;
 	
-     reg [15:0] period;
-	reg sq_wave_reg = 0;
+    reg [15:0] period;
+	reg [6:0] sq_wave_reg;
 	assign sq_wave = sq_wave_reg;
 
  	always @(posedge clk) begin
 		if (!reset_n | frequency == 0) begin
 			period <= 16'b0;
-			sq_wave_reg	<= 1'b0;
+			sq_wave_reg	<= 7'b0;
             period <= 16'b0;
 		end
 	
 		else begin 
 			if (period == 16'b0) begin
-				sq_wave_reg <= ~sq_wave_reg;
+				if (sq_wave_reg == 0)
+					sq_wave_reg <= amplitude * 2;
+				else 
+					sq_wave_reg <= 0;
 				period <= CLOCK_FREQUENCY / frequency - 1;		
 			end else period <= period - 1; 
 			end
