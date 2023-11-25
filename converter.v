9e90918950ec132abd7ae8600e22a14114ce428b
Tara Tafreshi <tara.tafreshi@mail.utoronto.ca>
2023-11-25 18:16:40 -0500

    converter

--- converter.v
+++ converter.v
@@ -1,12 +1,13 @@
 module converter(
     input[6:0] wave_in,
+input[6:0] amp,
     output reg[6:0] wave_out
 );
 
 always@(*) begin
-if(wave_in == d'0)
-    wav_out = wave_in/2;
+if(wave_in == 7'b0000000)
+    wave_out = ~amp+1;
 else
-    wave_out = $signed(wave_in/2);
+    wave_out = amp;
 end
 endmodule
