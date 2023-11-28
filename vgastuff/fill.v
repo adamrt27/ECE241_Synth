8855dc8343a20ab3e43945f14001a622fb5a859a
candicerayne <150631261+candicerayne@users.noreply.github.com>
2023-11-28 15:40:44 -0500

    Update fill.v

--- vgastuff/fill.v
+++ vgastuff/fill.v
@@ -85,6 +85,8 @@ defparam VGA.BACKGROUND_IMAGE = "piano.mif";
 
 // Put your code here. Your code should produce signals x,y,colour and writeEn
 // for the VGA controller, in addition to any other functionality your design may require.
-
+ps2 p0(
+  
+);
 
 endmodule
